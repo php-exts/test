e51ea894285885aa27cba1cf2cf62bea9f435a81
Cloudflying <oss@live.hk>
2020-11-16 14:06:46 +0800

    update demo

---
+++ src/Test.php
@@ -0,0 +1,21 @@
+<?php 
+namespace App
+
+/**
+ * Test Class
+ */
+class Test
+{
+	
+	public function __construct(argument)
+	{
+		# code...
+	}
+
+	/**
+	 * app method
+	 * @access public
+	 */
+	public function app(){}
+
+}
